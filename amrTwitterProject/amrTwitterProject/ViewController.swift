c086367155bfaeca9f356d04293ec050a2e35734
Nicolas Monzon <nicolasmonzon0@gmail.com>
2023-08-19 14:49:00 -0300

    initial twitter setup

---
+++ amrTwitterProject/amrTwitterProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  amrTwitterProject
+//
+//  Created by Nicolas Monzon on 19/8/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
